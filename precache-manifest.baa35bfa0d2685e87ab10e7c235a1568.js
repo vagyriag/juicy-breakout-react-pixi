7f64aafbac5e2158797c1b5e1bb387b64cf12de3
Sebastián Gaviria <sebastiangaviria@outlook.com>
2020-04-06 23:54:24 -0500

    Updates

---
+++ precache-manifest.baa35bfa0d2685e87ab10e7c235a1568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b9e0af85ed1bcb27c1d12207514cf2",
+    "url": "/juicy-breakout-react-pixi/index.html"
+  },
+  {
+    "revision": "566f0cb9c31e9197ea8c",
+    "url": "/juicy-breakout-react-pixi/static/css/main.547804c1.chunk.css"
+  },
+  {
+    "revision": "f0bd4cbc0ca321063439",
+    "url": "/juicy-breakout-react-pixi/static/js/2.d76ed714.chunk.js"
+  },
+  {
+    "revision": "040d413fe72da881b80c7fb17565924f",
+    "url": "/juicy-breakout-react-pixi/static/js/2.d76ed714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566f0cb9c31e9197ea8c",
+    "url": "/juicy-breakout-react-pixi/static/js/main.090c3803.chunk.js"
+  },
+  {
+    "revision": "bef79818d78659ca878d",
+    "url": "/juicy-breakout-react-pixi/static/js/runtime-main.ebca8f03.js"
+  }
+]);
